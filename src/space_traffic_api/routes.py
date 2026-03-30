1b9035e44ac54346b91745ea880e5eae16bf06eb
pwatrick <pwcantin@protonmail.com>
2026-03-30 18:53:17 -0500

    removed top level import shims

--- src/space_traffic_api/routes.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .api.routes import create_api_blueprint
-
-__all__ = ["create_api_blueprint"]
--- src/space_traffic_api/runtime.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .simulation.runtime import RuntimeState
-
-__all__ = ["RuntimeState"]
